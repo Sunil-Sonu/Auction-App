aa1e0088ca834d626e9a05f1e54f54762b1cf0d7
Sunil <sunil.sonu079@gmail.com>
2016-08-06 22:55:10 +0530

    Changes done

--- ExperimentAuction/settings.py
+++ ExperimentAuction/settings.py
@@ -145,19 +145,3 @@ MEDIA_URL = '/media/'
 
 STATIC_URL = '/static/'
 
-
-import dj_database_url
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
-
-
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
